69b50d5ba1ef6be0e06149aa75040b91aa98a328
AjmirSarker <arafat15-13512@diu.edu.bd>
2022-05-07 16:00:08 +0600

    putdelete

--- index.js
+++ index.js
@@ -6,28 +6,57 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z8sx0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1
 });
-async function run(){
-try{
-
-  await client.connect();
+async function run() {
+  try {
+    await client.connect();
     const productCollection = client.db('warehouse').collection('products');
-    app.get('/product',async(req,res)=>{
-      const query = {}
+    app.get('/products', async (req, res) => {
+      const query = {};
       const cursor = productCollection.find(query);
-      const items = await cursor.toArray()
-      res.send(items)
+      const items = await cursor.toArray();
+      res.send(items);
+    });
+    app.get('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
+    app.delete('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
+    app.post('/products', async (req, res) => {
+      const newProduct = req.body;
+      const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+    });
+    // update
+    app.put('/products/:id', async (req, res) => {
+      const id = req.params;
+      const updatedProduct = req.body;
+      console.log(updatedProduct);
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+          $set: {
+              quantity: updatedProduct.quantity
+          },
+      };
+      const result = await productCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
   })
-}
-finally{
-
-}
+  } finally {
+  }
 }
 run().catch(console.dir);
 app.get('/', (req, res) => {
